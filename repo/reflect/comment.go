f807c92b79e58dea5318e53c7b2fe87819c7f494
kalifun <kalifun@163.com>
2022-10-14 19:36:15 +0800

    :bug:  文本解析问题

--- repo/reflect/comment.go
+++ repo/reflect/comment.go
@@ -1,7 +1,6 @@
 package reflect
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/kalifun/proto-doc/entity/api"
@@ -15,22 +14,22 @@ import (
 func ConversionRpcComments(msg string) api.ApiDescribe {
 	var desc api.ApiDescribe
 	comments := make(map[string]string)
-	err := yaml.Unmarshal([]byte(msg), &comments)
+	text := TextPreProcessing(msg)
+	err := yaml.Unmarshal([]byte(text), &comments)
 	if err != nil {
-		fmt.Println(err)
 		return desc
 	}
-	if v, ok := comments["Path"]; ok {
-		desc.Path = v
+	if v, ok := comments["path"]; ok {
+		desc.Path = strings.ReplaceAll(strings.TrimSpace(v), "\n", "")
 	}
-	if v, ok := comments["Desc"]; ok {
-		desc.Desc = v
+	if v, ok := comments["desc"]; ok {
+		desc.Desc = strings.ReplaceAll(strings.TrimSpace(v), "\n", "")
 	}
-	if v, ok := comments["Version"]; ok {
-		desc.Version = v
+	if v, ok := comments["version"]; ok {
+		desc.Version = strings.ReplaceAll(strings.TrimSpace(v), "\n", "")
 	}
-	if v, ok := comments["Method"]; ok {
-		desc.Method = v
+	if v, ok := comments["method"]; ok {
+		desc.Method = strings.ReplaceAll(strings.TrimSpace(v), "\n", "")
 	}
 	return desc
 }
@@ -42,22 +41,36 @@ func ConversionRpcComments(msg string) api.ApiDescribe {
 func ConversionFieldComments(msg string) api.Param {
 	var param api.Param
 	comments := make(map[string]string)
-	err := yaml.Unmarshal([]byte(msg), &comments)
+	text := TextPreProcessing(msg)
+	err := yaml.Unmarshal([]byte(text), &comments)
 	if err != nil {
 		param.Desc = strings.ReplaceAll(strings.TrimSpace(msg), "\n", "")
 		param.Required = "UNKNOW"
 		return param
 	}
 
-	if v, ok := comments["Required"]; ok {
+	if v, ok := comments["required"]; ok {
 		param.Required = v
 	} else {
 		param.Required = "UNKNOW"
 	}
 
-	if v, ok := comments["Desc"]; ok {
+	if v, ok := comments["desc"]; ok {
 		param.Desc = strings.ReplaceAll(strings.TrimSpace(v), "\n", "")
 	}
 
 	return param
+}
+
+// TextPreProcessing
+//
+//	@param msg
+//	@return string
+func TextPreProcessing(msg string) string {
+	strs := []string{}
+	for _, v := range strings.Split(msg, "\n") {
+		strs = append(strs, strings.ToLower(strings.TrimPrefix(strings.TrimSpace(v), "@")))
+	}
+
+	return strings.Join(strs, "\n")
 }
